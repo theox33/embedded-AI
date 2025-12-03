99be7413ff4bdbbe8671277c2424e6803b93ada9
Théo Avril <theo.avril@reseau.eseo.fr>
2025-12-03 14:27:23 +0100

    initial commit

---
+++ step2/launch_python_object_detection.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Copyright (c) 2024 STMicroelectronics.
+# All rights reserved.
+#
+# This software is licensed under terms that can be found in the LICENSE file
+# in the root directory of this software component.
+# If no LICENSE file comes with this software, it is provided AS-IS.
+
+weston_user=$(ps aux | grep '/usr/bin/weston '|grep -v 'grep'|awk '{print $1}')
+FRAMEWORK=$1
+echo "stai_mpu wrapper used : "$FRAMEWORK
+CONFIG=$(find /usr/local/x-linux-ai -name "config_board_*.sh")
+source $CONFIG
+cmd="/usr/local/x-linux-ai/workspace/step1/stai_mpu_object_detection_starting_point.py -m /usr/local/x-linux-ai/workspace/models/yolov8s_integer_quant_256_1c_pingpongball_fp32_io.nb -l /usr/local/x-linux-ai/workspace/models/labels_coco_dataset_80_yolov8.txt --framerate $DFPS --frame_width $DWIDTH --frame_height $DHEIGHT --camera_src $CAMERA_SRC"
+
+if [ "$weston_user" != "root" ]; then
+	echo "user : "$weston_user
+	script -qc "su -l $weston_user -c '$cmd'"
+else
+	$cmd
+fi
